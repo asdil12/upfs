94aaf8e15b3ad97f4d3d87d2d2676fb8532b08ef
Dominik Heidler <dheidler@suse.de>
2022-02-18 03:07:49 +0100

    Add cache and read big amounts of data in chunks

--- upfs.py
+++ upfs.py
@@ -34,6 +34,7 @@ def rcall_bin(cmd):
 class UPFS(Operations):
 	def __init__(self):
 		self.fds = set()
+		self.cache = {}
 
 	# Helpers
 	# =======
@@ -45,6 +46,12 @@ class UPFS(Operations):
 		self.fds.add(i)
 		return i
 
+	def invalidate_cache(self, path):
+		try:
+			del self.cache[path]
+		except KeyError:
+			pass
+
 	# Filesystem methods
 	# ==================
 
@@ -55,13 +62,19 @@ class UPFS(Operations):
 		#raise FuseOSError(errno.EACCES)
 
 	def chmod(self, path, mode):
+		self.invalidate_cache(path)
 		print("ignoring chmod: %s: %o" % (path, mode))
 
 	def chown(self, path, uid, gid):
+		self.invalidate_cache(path)
 		print("ignoring chown: %s: %i / %i" % (path, uid, gid))
 
 	def getattr(self, path, fh=None):
-		print("getattr: %s" % path)
+		print("getattr: %s (%r)" % (path, fh))
+		if path in self.cache:
+			if self.cache[path] == errno.ENOENT:
+				raise FuseOSError(errno.ENOENT)
+			return self.cache[path]
 		"""
 		   S_IFSOCK   0140000   socket
            S_IFLNK    0120000   symbolic link
@@ -80,7 +93,7 @@ class UPFS(Operations):
 			#	st[0] |= 0o755
 			#else:
 			#	st[0] |= 0o644
-			return {
+			a = {
 				'st_mode': st[0],
 				# st_ino
 				# st_dev
@@ -93,9 +106,12 @@ class UPFS(Operations):
 				'st_mtime': st[8],
 				'st_ctime': st[9],
 			}
+			self.cache[path] = a
+			return a
 		except rshell.pyboard.PyboardError:
 			#pass
 			#raise FuseOSError(errno.EACCES)
+			self.cache[path] = errno.ENOENT
 			raise FuseOSError(errno.ENOENT)
 
 	def readdir(self, path, fh):
@@ -128,14 +144,17 @@ class UPFS(Operations):
 
 	def mknod(self, path, mode, dev):
 		print("mknod: %s" % path)
+		self.invalidate_cache(path)
 		raise FuseOSError(errno.EINVAL)
 
 	def rmdir(self, path):
 		print("rmdir: %s" % path)
+		self.invalidate_cache(path)
 		pyb.exec("os.rmdir(%r)" % path)
 
 	def mkdir(self, path, mode):
 		print("mkdir: %s" % path)
+		self.invalidate_cache(path)
 		pyb.exec("os.mkdir(%r)" % path)
 
 	def statfs(self, path):
@@ -173,28 +192,34 @@ class UPFS(Operations):
 
 	def unlink(self, path):
 		print("unlink: %s" % path)
+		self.invalidate_cache(path)
 		pyb.exec('os.remove(%r)' % path)
 
 	def symlink(self, name, target):
 		print("symlink: %s" % path)
+		self.invalidate_cache(path)
 		raise FuseOSError(errno.EINVAL)
 
 	def rename(self, old, new):
 		print("rename: %s" % path)
+		self.invalidate_cache(path)
 		pyb.exec('os.rename(%r, %r)' % (old, new))
 
 	def link(self, target, name):
 		print("link: %s" % path)
+		self.invalidate_cache(path)
 		raise FuseOSError(errno.EINVAL)
 
 	def utimens(self, path, times=None):
 		print("ignoring utimens: %s" % path)
+		self.invalidate_cache(path)
 		#return os.utime(self._full_path(path), times)
 
 	# File methods
 	# ============
 
 	def open(self, path, flags):
+		self.invalidate_cache(path)
 		print("open: %r" % path)
 		print("%x" % flags)
 		sf = ""
@@ -217,6 +242,7 @@ class UPFS(Operations):
 		return fd
 
 	def create(self, path, mode, fi=None):
+		self.invalidate_cache(path)
 		print("create: %r: %r" % (path, mode))
 		fd = self._get_new_fd()
 		sf = "wb"
@@ -225,15 +251,22 @@ class UPFS(Operations):
 		return fd
 
 	def read(self, path, length, offset, fd):
-		print("read: %r" % path)
+		print("read: %r (%i, %i)" % (path, length, offset))
+		self.invalidate_cache(path)
 		pyb.exec("upfs_fd_%i.seek(%i)" % (fd, offset))
-		print("upfs_fd_%i.read(%i)" % (fd, length))
-		r = rcall_bin("upfs_fd_%i.read(%i)" % (fd, length))
-		print("%r" % r)
+		# transfer in small chunks due to limited memory on device
+		r = b""
+		chunk_size = 8192
+		for i in range(0, length, chunk_size):
+			bytes_to_transfer = min(chunk_size, length-i)
+			print("upfs_fd_%i.read(%i)" % (fd, bytes_to_transfer))
+			r += rcall_bin("upfs_fd_%i.read(%i)" % (fd, bytes_to_transfer))
+		#print("%r" % r)
 		return r
 
 	def write(self, path, buf, offset, fd):
 		print("write %r: %r, %i" % (path, buf, offset))
+		self.invalidate_cache(path)
 		bb = base64.encodebytes(buf)
 		pyb.exec('import ubinascii')
 		pyb.exec("upfs_fd_%i.seek(%i)" % (fd, offset))
@@ -242,6 +275,7 @@ class UPFS(Operations):
 
 	def truncate(self, path, length, fd=None):
 		print("truncate %r" % path)
+		self.invalidate_cache(path)
 		tfd = self._get_new_fd()
 		pyb.exec("upfs_fd_%i = open(%r, 'rb')" % (tfd, path))
 		r = rcall_bin("upfs_fd_%i.read(%i)" % (tfd, length))
@@ -260,16 +294,19 @@ class UPFS(Operations):
 
 	def flush(self, path, fd):
 		print("flush %r" % path)
+		self.invalidate_cache(path)
 		pyb.exec("upfs_fd_%i.flush()" % fd)
 
 	def release(self, path, fd):
 		print("release: %r" % path)
+		self.invalidate_cache(path)
 		print("del upfs_fd_%i" % fd)
 		pyb.exec("del upfs_fd_%i" % fd)
 		self.fds.remove(fd)
 
 	def fsync(self, path, fdatasync, fd):
 		print("fsync %r" % path)
+		self.invalidate_cache(path)
 		pyb.exec("upfs_fd_%i.flush()" % fd)
 
 
